5b260c755925e307e9ac52fddea137868cb45b96
Devan Huapaya <huapayadevan@gmail.com>
2014-07-26 20:52:27 -0500

    Mostly Working 3d
    Still need to find a way to link on 'Read More' section

--- js/main.js
+++ js/main.js
@@ -20,6 +20,112 @@
     
 })(jQuery);
 
+
+/* THREE DEE EFFECTS */
+/* Variables */    
+var $3dcontainer = $('.threedeecontainer'),
+        $container = $('.container'),
+        $offsetable = $('.offsetable'),
+        $twisted = $('#threedeecontainer.twisted .offsetable'),
+        halfWidth = $offsetable.outerWidth()/2 + 'px',
+        sideView = false;
+
+  /* Set threedeecontainer to content height */
+  $3dcontainer.each(function(index){
+    // setHeight = $(this).find(".offsetable").height();
+    setHeight = $(this).height();
+    // alert(Number(setHeight));
+  });
+
+  /* Intial rotation */
+    $container.css({
+      '-webkit-transform-origin': halfWidth + ' 0 -' + halfWidth,
+      '-moz-transform-origin': halfWidth + ' 0 -' + halfWidth,
+      '-o-transform-origin': halfWidth + ' 0 -' + halfWidth,
+      'transform-origin': halfWidth + ' 0 -' + halfWidth
+    });
+    
+    /* Initial rotation */
+    $offsetable.css({
+      '-webkit-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+      '-moz-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+      '-o-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+      'transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')'
+    });
+  
+    /* Event Handler */ 
+    $3dcontainer.on('click', function(e) {
+      
+      /* If not turned */
+      if(sideView === false){
+        /* Turn left! */
+        $('.offsetable', this).css({
+          '-webkit-transform': 'translateZ(-' + halfWidth + ') rotateY(-90deg) translateZ(' + halfWidth + ')',
+          '-ms-transform': 'translateZ(-' + halfWidth + ') rotateY(-90deg) translateZ(' + halfWidth + ')',
+          '-o-transform': 'translateZ(-' + halfWidth + ') rotateY(-90deg) translateZ(' + halfWidth + ')',
+          'transform': 'translateZ(-' + halfWidth + ') rotateY(-90deg) translateZ(' + halfWidth + ')'
+        });
+        
+        sideView = true;
+      }
+      /* If turned */
+      else{
+        /* Turn right! */
+        $offsetable.css({
+          '-webkit-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+          '-ms-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+          '-o-transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')',
+          'transform': 'translateZ(-' + halfWidth + ') rotateY(0) translateZ(' + halfWidth + ')'
+        });
+        
+        sideView = false;
+      } 
+      
+      
+    }); 
+      
+      // var $c1 = $('.c1');
+      // $c1.each(function(i){
+      //   alert(i);
+      // });
+
+      $c1 = $('.c1');
+      $c2 = $('.c2');
+      var $c1off = $('.c1 > .offsetable');
+      var $c2off = $('.c2 > .offsetable ');
+      var $3d = $('.threedeecontainer');
+      // $c2off.outerWidth($c1off.outerWidth());
+      // $c2off.height($c1off.height());
+      // $3d.height($('.threedeecontainer > .c1 > .offsetable ').height());
+      // $c2.outerWidth($c1.outerWidth());
+      // $c2.height($c1.height());
+
+      $.each($c1off, function(i, value){
+        // c implies current
+        var c1index = $c1off.index($(this)),
+            thisC2 = $($c2off.get(c1index)),
+            this3d = $($3d.get(c1index));
+
+        this3d.height($(this).height());
+        thisC2.height($(this).height());
+        thisC2.outerWidth($(this).outerWidth());
+            // a??lert();
+
+        // c2c.height(c1c.height());
+        // c2c.outerWidth(c1c.outerWidth());
+      });
+      
+/*
+css changed to be dynamic:
+
+.container -> transform-origin: 100px 0 -100px;
+.offsetable -> transform: translateZ(-100px) rotateY(0) translateZ(100px);
+#threedeecontainer.twisted .offsetable -> transform: translateZ(-100px) rotateY(-90deg) translateZ(100px);
+
+*/
+
+
+
 var win = $(window);
 
 var allMods = $(".module");
